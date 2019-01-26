e020ef8a7124ccbb298f6979dfad5d72264eaba2
wgrh12 <wgrh99@yahoo.com>
2019-01-26 12:45:42 -0800

    Update code1.py, add timedelta

--- code1.py
+++ code1.py
@@ -1,3 +1,6 @@
 #!/usr/bin/env python
 print("Hello world!")
+from datetime import timedelta
 
+year = timedelta(days=365)
+year.total_seconds()
--- readme.txt
+++ readme.txt
@@ -1,1 +1,3 @@
-# git demo on 01/26/2019
+# readme.txt for git demo on 01/26/2019
+commit1
+Start a branch1
